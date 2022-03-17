777edeb7265845a3888e4a3861347975596ab661
John mwadime <johnniekips@gmail.com>
2022-03-17 13:22:18 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,147 +1,162 @@
-#install all of the required modules using pip
-#pip install pyttsx3
-#pip install wikipedia
-#pip install SpeechRecognition
-#pip install pygame
-#pip install pyown
-#pip install pyaudio
-
-#import necessary libraries
+#sudo apt-get install portaudio19-dev python-all-dev python3-all-dev
+#sudo apt-get install portaudio19-dev
+#pip install SpeechRecognition numpy gTTs sklearn 
+#pip install gTTS
+#sudo apt-get install mpg123
+ 
+import io
 import random
-import datetime
-import webbrowser
-import pyttsx3
-import wikipedia
-from pygame import mixer
-import speech_recognition as sr
-#Set up and calibrate the text to speech engine
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-volume = engine.getProperty('volume')
-engine.setProperty('volume', 10.0)
-rate = engine.getProperty('rate')
-engine.setProperty('rate', rate - 25)
-#Create lists with your preset questions answers
-greetings = ['hey there','heey', 'hello', 'hi', 'Hai', 'hey!', 'hey']
-question = ['How are you?', 'How are you doing?']
-responses = ['Okay', "I'm fine"]
-var1 = ['who made you', 'who created you']
-var2 = ['I_was_created_by_Edward_right_in_his_computer.', 'Edward', 'Some_guy_whom_i_never_got_to_know.']
-var3 = ['what time is it', 'what is the time', 'time']
-var4 = ['who are you', 'what is you name']
-cmd1 = ['open browser', 'open google']
-cmd2 = ['play music', 'play songs', 'play a song', 'open music player']
-cmd3 = ['tell a joke', 'tell me a joke', 'say something funny', 'tell something funny']
-jokes = ['Can a kangaroo jump higher than a house? Of course, a house doesn’t jump at all.', 'My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away.', 'Doctor: Im sorry but you suffer from a terminal illness and have only 10 to live.Patient: What do you mean, 10? 10 what? Months? Weeks?!"Doctor: Nine.']
-cmd4 = ['open youtube', 'i want to watch a video']
-cmd5 = ['tell me the weather', 'weather', 'what about the weather']
-cmd6 = ['exit', 'close', 'goodbye', 'nothing']
-cmd7 = ['what is your color', 'what is your colour', 'your color', 'your color?']
-colrep = ['Right now its rainbow', 'Right now its transparent', 'Right now its non chromatic']
-cmd8 = ['what is you favourite colour', 'what is your favourite color']
-cmd9 = ['thank you']
-repfr9 = ['youre welcome', 'glad i could help you']
-#Calibrate the speech recognition engine
-while True:
-    now = datetime.datetime.now()
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Tell me something:")
-        audio = r.listen(source)
-        try:
-            print("You said:- " + r.recognize_google(audio))
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-            engine.say('I didnt get that. Rerun the code')
-            engine.runAndWait()
-#Use a loop code with random and Wikipedia to finish the code
-if r.recognize_google(audio) in greetings:
-       random_greeting = random.choice(greetings)
-       print(random_greeting)
-       engine.say(random_greeting)
-       engine.runAndWait()
-   elif r.recognize_google(audio) in question:
-       engine.say('I am fine')
-       engine.runAndWait()
-       print('I am fine')
-   elif r.recognize_google(audio) in var1:
-       engine.say('I was made by edward')
-       engine.runAndWait()
-       reply = random.choice(var2)
-       print(reply)
-   elif r.recognize_google(audio) in cmd9:
-       print(random.choice(repfr9))
-       engine.say(random.choice(repfr9))
-       engine.runAndWait()
-   elif r.recognize_google(audio) in cmd7:
-       print(random.choice(colrep))
-       engine.say(random.choice(colrep))
-       engine.runAndWait()
-       print('It keeps changing every micro second')
-       engine.say('It keeps changing every micro second')
-       engine.runAndWait()
-   elif r.recognize_google(audio) in cmd8:
-       print(random.choice(colrep))
-       engine.say(random.choice(colrep))
-       engine.runAndWait()
-       print('It keeps changing every micro second')
-       engine.say('It keeps changing every micro second')
-       engine.runAndWait()
-   elif r.recognize_google(audio) in cmd2:
-       mixer.init()
-       mixer.music.load("C:\\Users\Edward Zion SAJI\Downloads\Mighty_God_-_Martin__Colleen_Rebeiro.55145718.wav")
-       mixer.music.play()
-   elif r.recognize_google(audio) in var4:
-       engine.say('I`m Scott your personal assistant')
-       engine.runAndWait()
-   elif r.recognize_google(audio) in cmd4:
-       webbrowser.open('www.youtube.com')
-   elif r.recognize_google(audio) in cmd6:
-       print('see you later')
-       engine.say('see you later')
-       engine.runAndWait()
-       exit()
-   elif r.recognize_google(audio) in cmd5:
-       owm = pyowm.OWM('YOUR_API_KEY')
-       observation = owm.weather_at_place('Bangalore, IN')
-       observation_list = owm.weather_around_coords(12.972442, 77.580643)
-       w = observation.get_weather()
-       w.get_wind()
-       w.get_humidity()
-       w.get_temperature('celsius')
-       print(w)
-       print(w.get_wind())
-       print(w.get_humidity())
-       print(w.get_temperature('celsius'))
-       engine.say(w.get_wind())
-       engine.runAndWait()
-       engine.say('humidity')
-       engine.runAndWait()
-       engine.say(w.get_humidity())
-       engine.runAndWait()
-       engine.say('temperature')
-       engine.runAndWait()
-       engine.say(w.get_temperature('celsius'))
-       engine.runAndWait()
-   elif r.recognize_google(audio) in var3:
+import string
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+from gtts import gTTS
+import os
+warnings.filterwarnings('ignore')
+import speech_recognition as sr 
+import nltk
+from nltk.stem import WordNetLemmatizer
 
-       print("Current date and time : ")
-       print(now.strftime("The time is %H:%M"))
-       engine.say(now.strftime("The time is %H:%M"))
-       engine.runAndWait()
-   elif r.recognize_google(audio) in cmd1:
-       webbrowser.open('www.google.com')
-   elif r.recognize_google(audio) in cmd3:
-       jokrep = random.choice(jokes)
-       engine.say(jokrep)
-       engine.runAndWait()
-   else:
-       engine.say("please wait")
-       engine.runAndWait()
-       print(wikipedia.summary(r.recognize_google(audio)))
-       engine.say(wikipedia.summary(r.recognize_google(audio)))
-       engine.runAndWait()
-       userInput3 = input("or else search in google")
-       webbrowser.open_new('www.google.com/search?q=' + userInput3)
- #     
+
+# for downloading package files can be commented after First run
+nltk.download('popular', quiet=True)
+nltk.download('nps_chat',quiet=True)
+nltk.download('punkt') 
+nltk.download('wordnet')
+
+
+posts = nltk.corpus.nps_chat.xml_posts()[:10000]
+
+# To Recognise input type as QUES. 
+def dialogue_act_features(post):
+    features = {}
+    for word in nltk.word_tokenize(post):
+        features['contains({})'.format(word.lower())] = True
+    return features
+
+featuresets = [(dialogue_act_features(post.text), post.get('class')) for post in posts]
+size = int(len(featuresets) * 0.1)
+train_set, test_set = featuresets[size:], featuresets[:size]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+# Recognised input types 
+#Greet
+#"Bye"/>
+#"Clarify"/>
+#"Continuer"/>
+#"Emotion"/>
+#"Emphasis"/>
+#"Greet"/>
+#"Reject"/>
+#"Statement"/>
+#"System"/>
+#"nAnswer"/>
+#"whQuestion"/>
+#"yAnswer"/>
+#"ynQuestion"/>
+#"Other"
+
+
+#colour palet
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk)) 
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk)) 
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
+
+
+ 
+#Reading in the input_corpus
+with open('intro_join','r', encoding='utf8', errors ='ignore') as fin:
+    raw = fin.read().lower()
+
+#TOkenisation
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
+# Preprocessing
+lemmer = WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+# Keyword Matching
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+
+def greeting(sentence):
+    """If user's input is a greeting, return a greeting response"""
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+# Generating response and processing 
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+
+#Recording voice input using microphone 
+file = "file.mp3"
+flag=True
+fst="My name is John. Welcome to Scott-Electronics."
+tts = gTTS(fst, 'en')
+tts.save(file)
+os.system("mpg123 " + file )
+r = sr.Recognizer()
+prYellow(fst)
+
+# Taking voice input and processing 
+while(flag==True):
+    with sr.Microphone() as source:
+        audio= r.listen(source)
+    try:
+        user_response = format(r.recognize(audio))
+        print("\033[91m {}\033[00m" .format("YOU SAID : "+user_response))
+    except sr.UnknownValueError:
+        prYellow("Oops! Didn't catch that")
+        pass
+    
+    #user_response = input()
+    #user_response=user_response.lower()
+    clas=classifier.classify(dialogue_act_features(user_response))
+    if(clas!='Bye'):
+        if(clas=='Emotion'):
+            flag=False
+            prYellow("John: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("\033[93m {}\033[00m" .format("John: "+greeting(user_response)))
+            else:
+                print("\033[93m {}\033[00m" .format("John: ",end=""))
+                res=(response(user_response))
+                prYellow(res)
+                sent_tokens.remove(user_response)
+                tts = gTTS(res, 'en')
+                tts.save(file)
+                os.system("mpg123 " + file)
+    else:
+        flag=False
+        prYellow("Jarvis: Bye! take care..")
